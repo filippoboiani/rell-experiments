d0459ecc341c56fdcb9e4127f85cf9536b2301d4
Filippo Boiani <filippo.boiani2@gmail.com>
2021-04-21 21:41:07 +0200

    added Test-Rell

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // using default postchain node REST API port
+  connectionUrl: 'http://localhost:7740',
+  // default blockchain identifier used for testing
+  blockchainRID: '6FF6C190BCDF00829C3AE0212D1C8C78A696FB81A32CA68C66A11EBCB9D0B3D2'
+}
