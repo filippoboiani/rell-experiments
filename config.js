4d53caf9e577cfde6acb7bc4101f2e6116a869db
Filippo Boiani <filippo.boiani2@gmail.com>
2021-04-22 10:10:50 +0200

    added new rell code

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   // using default postchain node REST API port
   connectionUrl: 'http://localhost:7740',
   // default blockchain identifier used for testing
-  blockchainRID: '6FF6C190BCDF00829C3AE0212D1C8C78A696FB81A32CA68C66A11EBCB9D0B3D2'
+  blockchainRID: '255F8FE0886492D793FBD2D6EFC7F1BF45A5DD8EA208C9195EA60B2A4443FAA8'
 }
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const pcl = require('postchain-client');
+const config = require('./config');
 
-const node_api_url = "http://localhost:7740"; // using default postchain node REST API port
-
-// default blockchain identifier used for testing
-// const blockchainRID = "78967baa4768cbcef11c508326ffb13a956689fcb6dc3ba17f4b895cbb1577a3";
-const blockchainRID = "6FF6C190BCDF00829C3AE0212D1C8C78A696FB81A32CA68C66A11EBCB9D0B3D2";
+const node_api_url = config.connectionUrl; // using default postchain node REST API port
+const blockchainRID = config.blockchainRID;
 
 const rest = pcl.restClient.createRestClient(node_api_url, blockchainRID, 5);
 const gtx = pcl.gtxClient.createClient(
@@ -19,18 +17,22 @@ const user = pcl.util.makeKeyPair();
 function add_cities() {
     const tx = gtx.newTransaction([user.pubKey]);
     tx.addOperation('insert_city', "Kiev");
-    tx.addOperation('insert_city', "Stockholm");
+    // tx.addOperation('insert_city', "Stockholm");
     tx.sign(user.privKey, user.pubKey);
     return tx.postAndWaitConfirmation();
 }
 
 function is_city_registered(city_name) {
-    return gtx.query("is_city_registered", {city_name: city_name});
-    
+    return gtx.query("is_city_registered", { city_name: city_name });
+}
+
+function get_all_cities() {
+    return gtx.query("get_all_cities", {});
 }
 
 async function runTest() {
     await add_cities();
+    // const res = await get_all_cities();
     const kiev_registered = await is_city_registered("Kiev");
     console.log("kiev_registered=", kiev_registered);
 }
